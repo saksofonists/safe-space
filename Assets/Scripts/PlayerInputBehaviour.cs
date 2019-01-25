3e4f2af63e2b010bf7648dec262fa3c560c75800
Toms Kusiņš <t.kusins@gmmail.com>
2019-01-25 23:01:09 +0200

    Player input

---
+++ Assets/Scripts/PlayerInputBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerInputBehaviour : MonoBehaviour {
+
+	public Vector2 Speed;
+
+	private void Update () {
+		var xdir = 0;
+		var ydir = 0;
+		if (Input.GetKey(KeyCode.A)) xdir--;
+		if (Input.GetKey(KeyCode.D)) xdir++;
+		if (Input.GetKey(KeyCode.W)) ydir++;
+		if (Input.GetKey(KeyCode.S)) ydir--;
+
+		transform.localPosition += (Vector3) (Speed * new Vector2(xdir, ydir) * Time.deltaTime);
+	}
+}
