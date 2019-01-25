feeaf9359dd0ca10eed71b3da1fd37c0ce4dc506
Toms Kusiņš <t.kusins@gmmail.com>
2019-01-26 01:41:06 +0200

    Menu screen

---
+++ Assets/Scripts/MenuButtonBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuButtonBehaviour : MonoBehaviour {
+
+    public Button NewGame;
+    public Button Quit;
+
+	// Use this for initialization
+	void Start () {
+		NewGame.onClick.AddListener(OnNewGame);
+		Quit.onClick.AddListener(OnQuit);
+	}
+
+	private void OnQuit() {
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+	}
+
+	private void OnNewGame() {
+		SceneManager.LoadScene("Scenes/Gameplay");
+	}
+}
