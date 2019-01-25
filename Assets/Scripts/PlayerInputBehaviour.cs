3bad9e5e909411c60235fd06e955a63f60f9e89d
Toms Kusiņš <t.kusins@gmmail.com>
2019-01-26 00:03:37 +0200

    Collision with walls n stuff

--- Assets/Scripts/PlayerInputBehaviour.cs
+++ Assets/Scripts/PlayerInputBehaviour.cs
@@ -1,17 +1,22 @@
 ﻿using UnityEngine;
 
 public class PlayerInputBehaviour : MonoBehaviour {
-
 	public Vector2 Speed;
+	private Rigidbody2D _body;
 
-	private void Update () {
+	private void Start() {
+		_body = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update() {
 		var xdir = 0;
 		var ydir = 0;
+
 		if (Input.GetKey(KeyCode.A)) xdir--;
 		if (Input.GetKey(KeyCode.D)) xdir++;
 		if (Input.GetKey(KeyCode.W)) ydir++;
 		if (Input.GetKey(KeyCode.S)) ydir--;
 
-		transform.localPosition += (Vector3) (Speed * new Vector2(xdir, ydir) * Time.deltaTime);
+		_body.velocity = Speed * new Vector2(xdir, ydir);
 	}
 }
