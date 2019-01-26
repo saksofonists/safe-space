f1684fd7ac8e7521eaf60f12fc7147ff300b16cc
Toms Kusiņš <t.kusins@gmmail.com>
2019-01-26 16:18:33 +0200

    Create Hp pickup script

---
+++ Assets/Scripts/HpPickup.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class HpPickup : MonoBehaviour, IPlayerWatcher {
+    public float Health;
+    private bool _ensureOnceJustInCase;
+    
+    public void Colliding(float timeDelta, PlayerBehaviour player) {
+        if (player.Health >= player.MaxHealth - 0.01f || _ensureOnceJustInCase) return;
+        _ensureOnceJustInCase = true;
+
+        player.Health += Health;
+        Destroy(gameObject);
+    }
+}
