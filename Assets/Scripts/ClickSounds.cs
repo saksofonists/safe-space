64bf52f481d106d1a3ae7054959a18b59c2ad017
Edgars Lapins <edgars.lapins9@gmail.com>
2019-01-27 12:42:27 +0200

    MenuButtonAudio

---
+++ Assets/Scripts/ClickSounds.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+public class ClickSounds : MonoBehaviour
+{
+    public AudioSource myFx;
+    public AudioClip hoverFx;
+    public AudioClip clickFx;
+
+        public void HoverSound()
+    {
+        myFx.PlayOneShot(hoverFx);
+    }
+    public void ClickSound()
+    {
+        myFx.PlayOneShot(clickFx);
+    }
+}
